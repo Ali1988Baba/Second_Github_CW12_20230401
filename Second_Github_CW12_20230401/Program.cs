de0d616c33e1b64ab103cada8a41aa159f9480c2
Ali1988Baba <alihaydarkutluk@hotmail.com>
2023-04-01 11:10:05 +0300

    Fix : add joker to delete \n Because it is not change anything in the programme

--- Second_Github_CW12_20230401/Program.cs
+++ Second_Github_CW12_20230401/Program.cs
@@ -11,7 +11,7 @@
             Console.Write("Soyisim Giriniz: ");
             string soyisim = Console.ReadLine();
 
-            Console.Write("\n1.Telefon Jokerinizi Giriniz: ");
+            Console.Write("1.Telefon Jokerinizi Giriniz: ");
             string telefonJoker1 = Console.ReadLine();
             Console.Write("2.Telefon Jokerinizi Giriniz: ");
             string telefonJoker2 = Console.ReadLine();
