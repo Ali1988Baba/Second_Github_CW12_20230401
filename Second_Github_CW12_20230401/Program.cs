daa2fc917f448f1376b7431d5e8524e37791b3b0
Ali1988Baba <alihaydarkutluk@hotmail.com>
2023-04-01 11:20:17 +0300

    Feat : add question draft section 1

--- Second_Github_CW12_20230401/Program.cs
+++ Second_Github_CW12_20230401/Program.cs
@@ -69,7 +69,17 @@
             // TODO: Daha iyi olabirlir mi? Her seferinde bunu tekrarlamaya gerek yok gibi.
             gizleA = false; gizleB = false;
             gizleC = false; gizleD = false;
-            #endregion
+        #endregion
+
+        #region Soru Kalıbı
+        soruNoktasi:
+            Console.Clear();
+            Console.WriteLine(soruNo + "-)" + soru);
+            Console.WriteLine("A) " + (!gizleA ? a : ""));
+            Console.WriteLine("B) " + (!gizleB ? b : ""));
+            Console.WriteLine("C) " + (!gizleC ? c : ""));
+            Console.WriteLine("D) " + (!gizleD ? d : ""));
+
 
         }
     }
