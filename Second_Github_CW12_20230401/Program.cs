88cec5cb95e9a807b588fe9d4f316d1763f7d759
Ali1988Baba <alihaydarkutluk@hotmail.com>
2023-04-01 11:02:22 +0300

    Add project files.

---
+++ Second_Github_CW12_20230401/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Second_Github_CW12_20230401
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
